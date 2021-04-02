f9fb7bda41290a3ab6cb8a018ba00f9bbdff4ddb
adoc-github <65012009+adoc-github@users.noreply.github.com>
2021-04-02 13:42:07 +0900

    Update wordpress.sh

--- applications/wordpress/wordpress.sh
+++ applications/wordpress/wordpress.sh
@@ -25,7 +25,7 @@ systemctl start apache2
 echo "****************************************************************"
 echo "Installing PHP Modules"
 echo "****************************************************************"
-apt-get install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd -y
+apt-get install php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd -y
 
 
 
